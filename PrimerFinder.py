91db1f071ebac7a2ef4d5a5bf175446623c62072
Alejandro Ruiz Bayón <alejandro_bayon@hotmail.com>
2021-01-26 13:13:38 +0100

    Added a function to calculate Tm
    The equation that calculates the Tm has been introduced in a function. The script asks the user to introduce a sequence, calculates its Tm using the function and returns its value and the execution time.

--- PrimerFinder.py
+++ PrimerFinder.py
@@ -1,5 +1,25 @@
 import math
+import time
 
-result = 100.5 + (41 * ((5+5)/(5+5+5+5))) - (820/(5+5+5+5)) + 16.6 * math.log10(0.05)
+def calculate_tm(sequence):
+    A = sequence.count("A")
+    T = sequence.count("T")
+    C = sequence.count("C")
+    G = sequence.count("G")
+
+    result = 100.5 + (41 * ((G+C)/(A+T+G+C))) - (820 / (A+T+G+C)) + 16.6 * math.log10(0.05)
+
+    return result
+
+sequence = input("enter the sequence: ")
+
+start = time.time()
+
+result = calculate_tm(sequence)
+
+end = time.time()
 
 print(result)
+print(end - start)
+
+#test: AGTCGATCGATACGTACCTGATCGTATCGATCGATCGATCGATGCATGACGTACGATCGTAGTAGCGTAGCTGATCGATCGTGATCGATGCTAGCTAGCGATCGATGCTAGCTAGCTAGCTAGCTCATCAG
